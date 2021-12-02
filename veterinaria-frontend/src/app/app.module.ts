39dc9f821f505f26e5463e3893d4fce541ace0d4
Jose Noguera <josenoguera1994@gmail.com>
2021-12-01 21:24:06 -0500

    Creando los componentes

--- veterinaria-frontend/src/app/app.module.ts
+++ veterinaria-frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MascotasComponent } from './components/mascotas/mascotas.component';
+import { CrearUsuarioComponent } from './components/crear-usuario/crear-usuario.component';
+import { EditarMascotaComponent } from './components/editar-mascota/editar-mascota.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    LoginComponent,
+    DashboardComponent,
+    MascotasComponent,
+    CrearUsuarioComponent,
+    EditarMascotaComponent
   ],
   imports: [
     BrowserModule,
